c086861a6f2877b72da8d509169d58e0db8dc9b3
Tatyana Lee <Tatyana Lee>
2016-08-30 10:26:56 -0500

    Completed

--- sketch.js
+++ sketch.js
@@ -3,11 +3,47 @@ function setup() {
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill('green');
-    noStroke();
-  }
-  ellipse(mouseX, mouseY, 10, 10);
+  ellipse(500, 500, 1000, 550);
+  ellipseMode(CENTER);
+  fill(0);
+  ellipse(500, 500, 900, 500);
+  ellipseMode(CENTER);
+  fill(255);
+  ellipse(500, 500, 800, 450);
+  ellipseMode(CENTER);
+  fill(0);
+  ellipse(500, 500, 700, 400);
+  ellipseMode(CENTER);
+  fill(255);
+  ellipse(500, 500, 600, 350);
+  ellipseMode(CENTER);
+  fill(0);
+  ellipse(500, 500, 500, 300);
+  ellipseMode(CENTER);
+  fill(255);
+  ellipse(500, 500, 400, 250);
+  ellipseMode(CENTER);
+  fill(0);
+  ellipse(500, 500, 300, 200);
+  ellipseMode(CENTER);
+  fill(255);
+  ellipse(500, 500, 200, 150);
+  ellipseMode(CENTER);
+  fill(0);
+  ellipse(500, 500, 100, 100);
+  ellipseMode(CENTER);
+  fill(255);
+
+  ellipse(100, 100, 80, 80);
+  ellipseMode(CENTER);
+  fill(255);
+  ellipse(800, 800, 80, 80);
+  ellipseMode(CENTER);
+  fill(0);
+  ellipse(100, 800, 80, 80);
+  ellipseMode(CENTER);
+  fill(0);
+  ellipse(800, 100, 80, 80);
+  ellipseMode(CENTER);
+  fill(0);
 }
